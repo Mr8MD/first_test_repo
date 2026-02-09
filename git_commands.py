e3bc9f0d33ae42264675f1b757f1f65e98c9c4cc
Mohammad Ali Ansari <mmansss.md228@gmail.com>
2026-02-09 06:31:27 +0330

    add GUI to the program

---
+++ git_commands.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+window = Tk()
+icon = PhotoImage(file='./not_code_files/git_with_glasses.png')
+button = Button(window, text='click on me')
+
+def click():
+    print('clicked')
+
+window.title("search git commands")
+window.geometry("1020x650")
+window.iconphoto(True, icon)
+window.config(background="#3c3a56")
+
+button.config(command=click)
+button.config(font=("Arial", 35, 'bold'))
+button.config(bg='#689a9d')
+button.config(fg='#ddffef')
+button.pack()
+
+
+window.mainloop()
